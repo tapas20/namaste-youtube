b8872387f32ea4e3b3f6ae4b97ee2b4408adf74a
Tapas Jyoti <tapasjyoti8327@gmail.com>
2024-11-19 12:01:15 +0530

    redux

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
 
 const Head = () => {
-  return (
-    <div>Head</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default Head
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+  return (
+    <div className="grid grid-flow-col p-2 m-2 shadow-lg">
+      <div className="flex gap-6 p-7 col-span-1">
+        <img
+          onClick={() => toggleMenuHandler()}
+          className="h-7 cursor-pointer"
+          src="https://www.svgrepo.com/show/336031/hamburger-button.svg"
+          alt="menu"
+        />
+        <a href="/">
+        <img
+          className="h-7"
+          src="https://sguru.org/wp-content/uploads/2018/02/2000px-YouTube_Logo_2017.svg_.png"
+          alt="Logo"
+        />
+        </a>
+      </div>
+      <div className="h-10 mt-7 col-span-10 text-center gap-10">
+        <input
+          placeholder="Search"
+          type="text"
+          className="w-1/2 px-5 p-2 rounded-l-full border border-gray-400"
+        />
+        <button className="hover:bg-gray-400 bg-gray-1.00 border border-gray-400 px-5 p-2 rounded-r-full">
+          🔍
+        </button>
+      </div>
+      <div>
+        <img
+          className="h-8 mt-7 col-span-1"
+          src="https://th.bing.com/th/id/R.c3631c652abe1185b1874da24af0b7c7?rik=XBP%2fc%2fsPy7r3HQ&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fpng-user-icon-circled-user-icon-2240.png&ehk=z4ciEVsNoCZtWiFvQQ0k4C3KTQ6wt%2biSysxPKZHGrCc%3d&risl=&pid=ImgRaw&r=0"
+          alt="user"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  const isMenuOpen = useSelector(store => store.app.isMenuOpen)
+  
+  // Early Return 
+  if(!isMenuOpen) return null;
 
-export default Sidebar
+  return (
+    <div className="p-7 shadow-lg w-48">
+      <ul>
+        <li className="font-bold">Home</li>
+        <li>Shorts</li>
+        <li>subscriptions</li>
+      </ul>
+
+      <h1 className="font-bold pt-7">You</h1>
+      <ul>
+        <li>Trending</li>
+        <li>Shopping</li>
+        <li>Music</li>
+        <li>Live</li>
+        <li>Gaming</li>
+        <li>News</li>
+        <li>Sports</li>
+        <li>Courses</li>
+        <li>Fashion & Beauty</li>
+        <li>Podcasts</li>
+      </ul>
+
+      <h1 className="font-bold pt-7">Explore</h1>
+      <ul>
+        <li>History</li>
+        <li>Playlists</li>
+        <li>Your videos</li>
+        <li>Your courses</li>
+        <li>Watch later</li>
+        <li>Liked video</li>
+      </ul>
+      <h1 className="font-semibold pt-7">Settings</h1>
+      <h1 className="font-semibold ">Report history</h1>
+      <h1 className="font-semibold ">Help</h1>
+      <h1 className="font-semibold ">Send feedback</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
